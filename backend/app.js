26a89736e58b19f7cbc023f584cb0c25aeda75dd
T-Charles <charlestettehnull@gmail.com>
2025-08-31 14:04:46 +0000

    Added authentication to check for tokens

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ import connectToDatabase from "./database/database.js";
 import PropertyOwnerRouter from "./routes/propertyowner.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import authenticateRenter from "./middleware/authenticaterenters.middleware.js";
+import authenticatePropertyOwner from "./middleware/authenticatepropertyowners.middleware.js";
 
 const app = express();
 
@@ -13,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/v1/renters/", RenterRouter);
-app.use("/api/v1/owners/", PropertyOwnerRouter);
+app.use("/api/v1/renters/", authenticateRenter, RenterRouter);
+app.use("/api/v1/owners/", authenticatePropertyOwner, PropertyOwnerRouter);
 
 app.listen(PORT, async () => {
 	console.log(`running on port ${PORT}`);
