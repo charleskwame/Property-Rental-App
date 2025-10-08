48566710c6940d3e34acae03fd89696992be45db
T-Charles <charlestettehnull@gmail.com>
2025-10-08 10:57:03 +0000

    fix: Rename DB_URI to MONGODB_URI for consistency in configuration

--- backend/config/env.js
+++ backend/config/env.js
@@ -5,7 +5,7 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 export const {
 	PORT,
 	NODE_ENV,
-	DB_URI,
+	MONGODB_URI,
 	JWT_SECRET,
 	JWT_EXPIRES_IN,
 	NODEMAILER_EMAIL,
--- backend/database/database.js
+++ backend/database/database.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-import { DB_URI, NODE_ENV } from "../config/env.js";
+import { MONGODB_URI, NODE_ENV } from "../config/env.js";
 
-if (!DB_URI) {
+if (!MONGODB_URI) {
 	throw new Error("Database URI not found in config file");
 }
 
 const connectToDatabase = async () => {
 	try {
-		await mongoose.connect(DB_URI);
+		await mongoose.connect(MONGODB_URI);
 		console.log(`Database connected successfully in ${NODE_ENV}`);
 	} catch (error) {
 		console.log(error);
