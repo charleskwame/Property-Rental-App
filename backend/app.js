a1b6d43f0506179d867ec66a89217fb4d2aa0063
T-Charles <charlestettehnull@gmail.com>
2025-10-09 09:16:47 +0000

    fix: comment out unused cors import in app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import connectToDatabase from "./database/database.js";
 //import PropertyOwnerRouter from "./routes/user.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cors from "cors";
 import UserRouter from "./routes/user.routes.js";
 //import authenticateRenter from "./middleware/authenticaterenters.middleware.js";
 //import authenticatePropertyOwner from "./middleware/authenticatepropertyowners.middleware.js";
