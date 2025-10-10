86a43cb513eb2898bc7829a083a420946984bab7
T-Charles <charlestettehnull@gmail.com>
2025-10-10 12:10:25 +0000

    fix: center loading message text in loading spinner component

--- frontend/src/components/loadingspinner.component.tsx
+++ frontend/src/components/loadingspinner.component.tsx
@@ -9,7 +9,7 @@ export default function LoadingSpinner({ message }: LoadingMessage) {
 				<div
 					className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-fuchsia-800 motion-reduce:animate-[spin_1.5s_linear_infinite]"
 					role="status"></div>
-				<span className="text-fuchsia-800 font-semibold text-sm">{message}</span>
+				<span className="text-fuchsia-800 font-semibold text-sm text-center">{message}</span>
 			</div>
 		</>
 	);
